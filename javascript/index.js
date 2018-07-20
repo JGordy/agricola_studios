373f5dbe104e50df0d0b1341f73af007d1de7c65
Joseph Gordy <jgordy2424@gmail.com>
2018-07-19 22:08:57 -0400

    Initial commit

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,4 @@
-
+let header = document.getElementById('header'),
+    skills = document.getElementById('skills'),
+    work   = document.getElementById('work'),
+    footer = document.getElementById('footer');
